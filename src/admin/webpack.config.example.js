718baea4401ce73bc67424b018887eb49f140a99
Ivan <iv@Ivans-MacBook-Air.local>
2022-08-03 15:36:39 +0300

    first commit

---
+++ src/admin/webpack.config.example.js
@@ -0,0 +1,22 @@
+export default {
+  config: {
+    // Replace the Strapi logo in auth (login) views
+
+    // Override or extend the theme
+    theme: {
+      colors: {
+        primary100: '#000',
+        primary200: '#000',
+        primary500: '#000',
+        primary600: '#000',
+        primary700: '#000',
+        danger700: '#000'
+      },
+    },
+    // Extend the translations
+
+  },
+
+  bootstrap() {},
+};
+
